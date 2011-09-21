257fcb765630a33c2b9c9a7ecac456b37de885ca
jihu.wang@gmail.com <jihu.wang@gmail.com@32301796-4f0f-5e80-19a8-b2862c7a4129>
2011-09-21 16:04:23 +0000

    添加私信文件

--- trunk/channel/service/player.manager.js
+++ trunk/channel/service/player.manager.js
@@ -1,39 +1,39 @@
 var common = require('../common/channel.common.js');
 var querystring = require('querystring');
 
-(function() {
+void function(){
 	/**
- 	* 玩家集合
- 	*/
+	 * 玩家集合
+	 */
 	var playerDict = {};
 	/**
- 	* 可更新的字段列表
- 	*/
+	 * 可更新的字段列表
+	 */
 	var updateFields = ['nick', 'state'];
 	/**
 	 * 昵称词典，避免昵称重复
 	 */
 	var nickDict = {};
 	/**
- 	* 获取用户身份验证码
- 	* @param {String} id 用户id
- 	* @param {String} visa 密码
- 	*/
-	function getPlayerMask(id, visa) {
+	 * 获取用户身份验证码
+	 * @param {String} id 用户id
+	 * @param {String} visa 密码
+	 */
+	function getPlayerMask(id, visa){
 		return (common.passportKey ^ parseInt(visa, 36) ^ parseInt(id, 36)).toString(36);
 	}
 	/**
 	 * 用户信息
 	 * @param {String} id
 	 */
-	function Player(id) {
+	function Player(id){
 		this.id = id || (+new Date - new Date('2011/8/16')).toString(36);
 		this.visa = parseInt(Math.random() * 99999999).toString(36);
 		this.mask = getPlayerMask(this.id, this.visa);
 		/**
 		 * 昵称
 		 */
-		this.nick = "@" + this.id;
+		this.nick = this.id;
 		/**
 		 * 在线状态 online-在线 offline-离线 busy-忙碌
 		 */
@@ -59,16 +59,16 @@ var querystring = require('querystring');
 		 * 最后发送命令的时间，用来验证是否活跃
 		 */
 		this.commandTime = now;
-
+		
 		playerDict[this.id] = this;
 	}
 	/**
 	 * 更新用户信息
 	 * @param {Object} data 更新的数据
 	 */
-	Player.prototype.update = function(data) {
+	Player.prototype.update = function(data){
 		var self = this;
-		common.forEach(updateFields, function(field) {
+		common.forEach(updateFields, function(field){
 			if (field in data) {
 				self[field] = data[field];
 				self.modifyTime = new Date;
@@ -79,28 +79,26 @@ var querystring = require('querystring');
 	 * 获取用户信息
 	 * @param {String} id 用户id
 	 */
-	function getPlayer(id) {
+	function getPlayer(id){
 		var player = playerDict[id];
-		if (!player)
-			return;
+		if (!player) return;
 		player.accessTime = new Date;
 		return player;
 	}
-
+	
 	/**
 	 * 根据cookie获取用户
 	 * @param{Object} req 请求对象
 	 * @param{Object} res 应答对象，如果为空，则不会创建不存在的用户
 	 */
-	function getPassport(req, res) {
+	function getPassport(req, res){
 		var cookie = req.headers['cookie'] || "";
 		var m = cookie.match(/\bpassport=([^;]+)/);
 		var passport = m && querystring.parse(m[1]);
 		var player = passport && getPlayer(passport.id);
 		if (!player || player.visa != passport.visa ||
-			player.mask != getPlayerMask(passport.id, passport.visa)
-		) {
-			if (!res) return;  
+		player.mask != getPlayerMask(passport.id, passport.visa)) {
+			if (!res) return;
 			player = new Player();
 			res.setHeader("Set-Cookie", [common.format("passport=id=#{id}&visa=#{visa}&mask=#{mask}; expires=Mon, 31 Dec 2998 16:00:00 GMT; path=/;", player)]);
 		} else {
@@ -108,8 +106,8 @@ var querystring = require('querystring');
 		}
 		return player;
 	};
-
+	
 	exports.getPassport = getPassport;
 	exports.getPlayer = getPlayer;
-
-})();
+	
+}();
