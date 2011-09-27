7404e807082ff13c35bd82211df6741b87ed3ddb
jihu.wang@gmail.com <jihu.wang@gmail.com@32301796-4f0f-5e80-19a8-b2862c7a4129>
2011-09-27 06:33:18 +0000

    完善私信功能

--- channel/browser/modules/letter-box.js
+++ channel/browser/modules/letter-box.js
@@ -1,35 +1,35 @@
 /**
  * @author 王集鹄(wangjihu，http://weibo.com/zswang)
  */
-AceCore.addModule("LetterBox", function(sandbox) {
+AceCore.addModule("LetterBox", function(sandbox){
 	/**
- 	* 事件集合
- 	*/
+	 * 事件集合
+	 */
 	var events = sandbox.getConfig("Events");
 	/**
- 	* 类库
- 	*/
+	 * 类库
+	 */
 	var lib = sandbox.getLib();
 	/**
- 	* 登录信息
- 	*/
+	 * 登录信息
+	 */
 	var passportInfo = {};
 	/**
- 	* 聊天室api
- 	*/
+	 * 聊天室api
+	 */
 	var chatApi = sandbox.getExtension("ChatApi");
 	/**
- 	* 私信列表
- 	*/
+	 * 私信列表
+	 */
 	var letterTree;
-
+	
 	/**
- 	* 获取房间当前状态成功
- 	* @param {Object} data
- 	*/
-	function pickSuccess(data) {
-		lib.each(data, function(item) {
-			switch(item.type) {
+	 * 获取房间当前状态成功
+	 * @param {Object} data
+	 */
+	function pickSuccess(data){
+		lib.each(data, function(item){
+			switch (item.type) {
 				case "passport":
 					passportInfo = item.info;
 					break;
@@ -44,54 +44,52 @@ AceCore.addModule("LetterBox", function(sandbox) {
 			}
 		});
 	}
-
+	
 	/**
- 	* 滚动到底部
- 	*/
-	function scrollBottom() {
+	 * 滚动到底部
+	 */
+	function scrollBottom(){
 		var parent = letterTree.parent.parentNode;
 		parent.scrollTop = parent.scrollHeight;
 	}
-
+	
 	/**
- 	* 是否是自己的账号
- 	* @param {String} id
- 	*/
-	function ifSelf(id) {
+	 * 是否是自己的账号
+	 * @param {String} id
+	 */
+	function ifSelf(id){
 		return id == passportInfo.id ? "self" : "";
 	}
-
+	
 	/**
- 	* 格式化事件
- 	* @param {Date} time
- 	*/
-	function formatTime(time) {
+	 * 格式化时间
+	 * @param {Date} time
+	 */
+	function formatTime(time){
 		time = new Date(time);
 		var timeStr = lib.date.format(time, "HH:mm:ss");
 		var dateStr = lib.date.format(time, "yyyy:MM:dd");
-		return lib.date.format(new Date, "yyyy:MM:dd") == dateStr ? timeStr :
-		[dateStr, timeStr].join(" ");
+		return lib.date.format(new Date, "yyyy:MM:dd") == dateStr ? timeStr : [dateStr, timeStr].join(" ");
 	}
-
+	
 	/**
- 	* 处理多行文本
- 	* @param {String} text 文本
- 	*/
-	function mutiline(text) {
+	 * 处理多行文本
+	 * @param {String} text 文本
+	 */
+	function mutiline(text){
 		return lib.encodeHTML(text).replace(/\n/g, "<br/>");
 	}
-
+	
 	/**
- 	* 发送私信对话框
- 	*/
-	function letterDialog(data) {
+	 * 发送私信对话框
+	 */
+	function letterDialog(data){
 		sandbox.fire(events.showDialog, {
 			type: "letter",
 			nick: data.nick,
 			to: data.to,
-			oncommand: function(command, data) {
-				if (command != "ok")
-					return;
+			oncommand: function(command, data){
+				if (command != "ok") return;
 				var letter = lib.g("inputLetter").value;
 				var error = ChannelCommon.checkLetter(letter);
 				if (error) {
@@ -106,32 +104,32 @@ AceCore.addModule("LetterBox", function(sandbox) {
 					to: data.to,
 					text: letter
 				});
-
+				
 			},
-			onshow: function(data) {
+			onshow: function(data){
 				var input = lib.g("inputLetter");
 				input.setSelectionRange(0, input.value.length);
 				input.focus();
 			}
 		});
 	}
-
-	function viewLetter() {
+	
+	function viewLetter(){
 		lib.removeClass('letterBox', 'hidden');
 	}
-
+	
 	return {
-		init: function() {
-
+		init: function(){
+		
 			letterTree = AceTree.create({
 				parent: lib.g("letterListTemplate").parentNode,
-				oninit: function(tree) {
-					tree.eventHandler = AceEvent.on(tree.parent, function(command, element, e) {
+				oninit: function(tree){
+					tree.eventHandler = AceEvent.on(tree.parent, function(command, element, e){
 						var node = tree.node4target(element);
 						node && tree.oncommand(command, node, e);
 					});
 				},
-				onreader: function(node) {
+				onreader: function(node){
 					return AceTemplate.format('letterListTemplate', node.data, {
 						node: node,
 						formatTime: formatTime,
@@ -139,7 +137,7 @@ AceCore.addModule("LetterBox", function(sandbox) {
 						mutiline: mutiline
 					});
 				},
-				oncommand: function(command, node, e) {
+				oncommand: function(command, node, e){
 					switch (command) {
 						case "reply":
 							sandbox.fire(events.letterDialog, {
@@ -150,12 +148,12 @@ AceCore.addModule("LetterBox", function(sandbox) {
 					}
 				}
 			});
-
+			
 			sandbox.on(events.letterDialog, letterDialog);
 			sandbox.on(events.pickSuccess, pickSuccess);
 			sandbox.on(events.viewLetter, viewLetter);
-
-			AceEvent.on('letterBox', function(command) {
+			
+			AceEvent.on('letterBox', function(command){
 				switch (command) {
 					case "ok":
 					case "cancel":
