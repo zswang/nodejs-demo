bab44eb07085963a1c5c13dae8fcfb970d5b3801
jihu.wang@gmail.com <jihu.wang@gmail.com@32301796-4f0f-5e80-19a8-b2862c7a4129>
2012-01-31 14:05:40 +0000

    精简代码

--- trunk/cscript/run.js
+++ trunk/cscript/run.js
@@ -1,28 +1,29 @@
 /*@cc_on
-@if(1)
-	var console;
-	function require(namespace){
-		var fso = new ActiveXObject('Scripting.FileSystemObject');
-		var filename = namespace;
-		var result = {};
-		if (/\w+/.test(namespace)){
-			filename = 'windows\\' + namespace + '.js';
-		}
-		if (!fso.FileExists(filename)) return;
-		var file = fso.GetFile(filename);
-		if (file.Size <= 0) return;
-		var stream = file.OpenAsTextStream(1, 0);
-		try{
-			new Function('exports', 'require', 'console', stream.ReadAll())(
-				result, require, console
-			);
-		} catch(ex){
-		} finally{
-			stream.Close();
-		}
-		return result;
+var console, JSON, process;
+function require(namespace){
+	var fso = new ActiveXObject('Scripting.FileSystemObject');
+	var filename = namespace;
+	var result = {};
+	if (/\w+/.test(namespace)){
+		filename = 'windows\\' + namespace + '.js';
 	}
-	console = require('console');
-@end@*/
+	if (!fso.FileExists(filename)) return;
+	var file = fso.GetFile(filename);
+	if (file.Size <= 0) return;
+	var stream = file.OpenAsTextStream(1, 0);
+	try{
+		new Function('exports', 'require', 'console', 'JSON', 'process', stream.ReadAll())(
+			result, require, console, JSON, process
+		);
+	} catch(ex){
+	} finally{
+		stream.Close();
+	}
+	return result;
+}
+JSON = require('JSON');
+process = require('process');
+console = require('console');
+@*/
 
 console.log('--hello %j--', { name: 'zswang'});
