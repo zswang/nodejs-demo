d842bdcf6188981138619ed85cf231339a92e2a9
jihu.wang@gmail.com <jihu.wang@gmail.com@32301796-4f0f-5e80-19a8-b2862c7a4129>
2012-01-31 14:01:46 +0000

    添加util.js和JSON.js适配

--- trunk/cscript/windows/fs.js
+++ trunk/cscript/windows/fs.js
@@ -1,26 +1,30 @@
-function readFile(filename/*, [encoding], [callback]*/){
-	var callback, err, data;
-	for (var i = 1; i < arguments.length; i++){
-		if (/^f/.test(typeof arguments[i])){
-			callback = arguments[i];
-		}
-	}
-	
-	var fso = new ActiveXObject('Scripting.FileSystemObject');
-	if (fso.FileExists(filename)){
-		var file = fso.GetFile(filename);
-		if (file.Size > 0){
-			var stream = file.OpenAsTextStream(1, 0);
-			try{
-				data = stream.ReadAll();
-			} catch(ex){
-				err = ex.message;
-			} finally{
-				stream.Close();
+var LocalFS = /^u/.test(typeof exports) ? LocalFS || {} : exports;
+
+void function(exports){
+	function readFile(filename/*, [encoding], [callback]*/){
+		var callback, err, data;
+		for (var i = 1; i < arguments.length; i++){
+			if (/^f/.test(typeof arguments[i])){
+				callback = arguments[i];
 			}
 		}
+		
+		var fso = new ActiveXObject('Scripting.FileSystemObject');
+		if (fso.FileExists(filename)){
+			var file = fso.GetFile(filename);
+			if (file.Size > 0){
+				var stream = file.OpenAsTextStream(1, 0);
+				try{
+					data = stream.ReadAll();
+				} catch(ex){
+					err = ex.message;
+				} finally{
+					stream.Close();
+				}
+			}
+		}
+		callback && callback(err, data);
 	}
-	callback && callback(err, data);
-}
 
-exports.readFile = readFile;
+	exports.readFile = readFile;
+}(LocalFS);
